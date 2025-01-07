e9c311e058e496f1703a6e44b295bff2a6aafd41
Descus <idescusi@gmail.com>
2025-01-08 00:19:33 +0100

    Add reflection to locate methods with attribute

--- PlanlosBot/BotHost.cs
+++ PlanlosBot/BotHost.cs
@@ -1,7 +1,10 @@
-﻿using Discord;
+﻿using System.Reflection;
+using Discord;
 using Discord.Interactions;
 using Discord.WebSocket;
+using PlanlosBot.Attributes;
 using PlanlosBot.Contracts;
+using PlanlosBot.Interactions.Modals;
 using PlanlosBot.Logging;
 
 namespace PlanlosBot;
@@ -12,7 +15,6 @@ public class BotHost
     private readonly InteractionService _interactionService;
     private readonly ILogWrapper _logWrapper;
     private readonly IServiceProvider _serviceProvider;
-    private readonly DiscordLogWrapper _discordLogWrapper;
 
     public BotHost(DiscordSocketClient socketClient, InteractionService interactionService , Func<string, ILogWrapper> loggerFactory, IServiceProvider serviceProvider)
     {
@@ -20,38 +22,63 @@ public class BotHost
         _interactionService = interactionService;
         _logWrapper = loggerFactory(nameof(BotHost));
         _serviceProvider = serviceProvider;
-        _discordLogWrapper = new DiscordLogWrapper(_logWrapper);
     }
 
     public async Task Launch()
     {
-        _socketClient.Log += _discordLogWrapper.Log;
-        _interactionService.Log += _discordLogWrapper.Log;
+        DiscordLogWrapper discordLogWrapper = new (_logWrapper); 
+        _socketClient.Log += discordLogWrapper.Log;
+        _interactionService.Log += discordLogWrapper.Log;
 
         await _interactionService.AddModulesAsync(typeof(BotHost).Assembly, _serviceProvider);
         
         await _socketClient.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DISCORD_TOKEN"));
         await _socketClient.StartAsync();
 
-        _socketClient.Ready += async () =>
-        {
-#if DEBUG
-            await _interactionService.RegisterCommandsToGuildAsync(1032990109101981788);
-#else
-            await _interactionService.RegisterCommandsGloballyAsync();
-#endif
-            await _logWrapper.Log("Bot started");
-        };
+        await FindEventAttributes();
         
         _socketClient.InteractionCreated += async interaction =>
         {
             IInteractionContext context = new SocketInteractionContext(_socketClient, interaction);
             await _interactionService.ExecuteCommandAsync(context, _serviceProvider);
         };
-        
         await Task.Delay(-1);
     }
     
     
+    public async Task RegisterCommands()
+    {
+#if DEBUG
+        await _interactionService.RegisterCommandsToGuildAsync(1032990109101981788);
+#else
+            await _interactionService.RegisterCommandsGloballyAsync();
+#endif
+        await _logWrapper.Log("Bot started");
+    }
 
+    private Task FindEventAttributes()
+    {
+        List<Task> tasks =
+        [
+            FindOnReady()
+        ];
+        return Task.WhenAll(tasks);
+    }
+
+    private Task FindOnReady()
+    {
+        MethodInfo[] methodInfos = typeof(BotHost).Assembly.GetTypes()
+            .SelectMany(t => t.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+            .Where(m => m.GetCustomAttributes(typeof(OnReadyAttribute), true).Length > 0).ToArray();
+        
+        if (methodInfos.Length == 0) 
+            return Task.CompletedTask;
+        
+        foreach (MethodInfo methodInfo in methodInfos)
+        {
+            object o = _serviceProvider.GetService(typeof(NameModal));
+            _socketClient.Ready += (Func<Task>)methodInfo.CreateDelegate(typeof(Func<Task>), o);
+        }
+        return Task.CompletedTask;
+    }
 }
